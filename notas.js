9758b6a3829b1393c71e764347bbcc317253dd10
dalvarezcg <96488708+dalvarezcg@users.noreply.github.com>
2025-03-07 17:00:12 -0300

    Add files via upload
    primer commit

---
+++ notas.js
@@ -0,0 +1,37 @@
+const notas = [
+    { nombre: "nota 1" },
+    { nombre: "nota 2" }
+];
+
+const notesContainer = document.getElementById("notesContainer");
+//const noteInput = document.getElementById("noteInput");
+
+
+function renderNotas() {
+notesContainer.innerHTML = ""; // Limpiar antes de volver a renderizar
+notas.forEach((nota, index) => {
+    const noteDiv = document.createElement("div");//crea un div
+    noteDiv.classList.add("note");
+    noteDiv.innerHTML = `
+        <span>${nota.nombre}</span>
+        <button onclick="deleteNote(${index})"><i class="bi bi-trash3"></i></button>
+    `;
+    notesContainer.appendChild(noteDiv);
+});
+}
+
+function addNote()
+{
+    event.preventDefault() // Prevenir la recarga de la página
+    var datosNota = document.getElementById('noteInput').value;
+    notas.push({ nombre: datosNota});
+    noteInput.value = "";
+    renderNotas();
+}
+
+// Eliminar nota
+function deleteNote(index) {
+    notas.splice(index, 1);
+    renderNotas();
+}
+renderNotas();
